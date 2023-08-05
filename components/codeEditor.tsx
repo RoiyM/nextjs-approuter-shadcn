13ce34960cfe7437e94ef130ff514e5e9d84401e
roiy <roiy355@gmail.com>
2023-08-05 15:05:41 +0300

    websocket

--- components/codeEditor.tsx
+++ components/codeEditor.tsx
@@ -1,9 +1,13 @@
 "use client";
+// import Editor from "@monaco-editor/react";
+import * as Y from "yjs";
+import { WebrtcProvider } from "y-webrtc";
+import { MonacoBinding } from "y-monaco";
 import { useRef } from "react";
-import Editor from "@monaco-editor/react";
-// import * as Y from "yjs";
-// import { WebrtcProvider } from "y-webrtc";
-// import { MonacoBinding } from "y-monaco";
+
+import dynamic from "next/dynamic";
+
+const Editor = dynamic(() => import("@monaco-editor/react"), { ssr: false });
 
 interface CodeEditorProps {
   id: string;
@@ -21,19 +25,20 @@ export default function CodeEditor({ id }: CodeEditorProps) {
   function handleEditorDidMount(editor: any, monaco: any) {
     editorRef.current = editor;
 
-    // // Initialize YJS
-    // const doc = new Y.Doc(); // a collection of shared objects -> Text
-    // // Connect to peers (or start connection) with WebRTC
-    // const provider = new WebrtcProvider(`room-${id}`, doc); // room1, room2
-    // const type = doc.getText("monaco"); // doc { "monaco": "what our IDE is showing" }
-    // // Bind YJS to Monaco
-    // const binding = new MonacoBinding(
-    //   type,
-    //   editorRef.current.getModel(),
-    //   new Set([editorRef.current]),
-    //   provider.awareness
-    // );
-    // console.log(provider.awareness);
+    // Initialize YJS
+    const doc = new Y.Doc(); // a collection of shared objects -> Text
+    // Connect to peers (or start connection) with WebRTC
+
+    const provider = new WebrtcProvider(`room${id}`, doc); // room1, room2
+    const type = doc.getText("monaco"); // doc { "monaco": "what our IDE is showing" }
+    // Bind YJS to Monaco
+    const binding = new MonacoBinding(
+      type,
+      editorRef.current.getModel(),
+      new Set([editorRef.current]),
+      provider.awareness
+    );
+    console.log(provider.awareness);
   }
 
   return (
